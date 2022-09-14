f6fda046ab834cabf9040bb01eac7195d7b4ccd7
Héctor Figueroa <69779567+Hejefica@users.noreply.github.com>
2022-09-13 22:42:34 -0500

    idQuestion DB working.

--- GuessWho.py
+++ GuessWho.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template, request
 import random
 import numpy as np
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password, database):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password,
+            database = database
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+    return connection
+
+def get_DB_info(connection):
+    cursor = connection.cursor()
+    query = 'select idQuestions from questions'
+    cursor.execute(query)
+
+    idQuestions = []
+
+    for row in cursor:
+        for field in row:
+            idQuestions.append(field)
+    return idQuestions
 
 app = Flask(__name__)
 
@@ -31,11 +59,18 @@ characters = [
 questions_so_far = []
 answers_so_far = []
 
-def calculate_character_probability(character, questions_so_far, answers_so_far):
-    # Prior
-    P_character = 1 / len(characters)
+def calculate_probabilites(questions_so_far, answers_so_far):
+    probabilities = []
+    for character in characters:
+        probabilities.append({
+            'name': character['name'],
+            'probability': calculate_character_probability(character, questions_so_far, answers_so_far)
+        })
 
-    # Likelihood
+    return probabilities
+
+def calculate_character_probability(character, questions_so_far, answers_so_far):
+    P_character = 1 / len(characters)
     P_answers_given_character = 1
     P_answers_given_not_character = 1
     for question, answer in zip(questions_so_far, answers_so_far):
@@ -47,14 +82,12 @@ def calculate_character_probability(character, questions_so_far, answers_so_far)
                                           if not_character['name'] != character['name']])
         P_answers_given_not_character *= max(P_answer_not_character, 0.01)
 
-    # Evidence
     P_answers = P_character * P_answers_given_character + \
         (1 - P_character) * P_answers_given_not_character
-
+    
     # Bayes Theorem
     P_character_given_answers = (
         P_answers_given_character * P_character) / P_answers
-
     return P_character_given_answers
 
 def character_answer(character, question):
@@ -62,18 +95,10 @@ def character_answer(character, question):
         return character['answers'][question]
     return 0.5
 
-def calculate_probabilites(questions_so_far, answers_so_far):
-    probabilities = []
-    for character in characters:
-        probabilities.append({
-            'name': character['name'],
-            'probability': calculate_character_probability(character, questions_so_far, answers_so_far)
-        })
-
-    return probabilities
-
 @app.route('/')
 def index():
+    connection = create_server_connection("localhost", "root", "hejefica", "guesswho")
+    idQuestions = get_DB_info(connection)
     global questions_so_far, answers_so_far
 
     question = request.args.get('question')
@@ -81,18 +106,17 @@ def index():
     if question and answer:
         questions_so_far.append(int(question))
         answers_so_far.append(float(answer))
-
     probabilities = calculate_probabilites(questions_so_far, answers_so_far)
-    print("probabilities", probabilities)
 
-    questions_left = list(set(questions.keys()) - set(questions_so_far))
+    questions_left = list(set(idQuestions) - set(questions_so_far))
+
     if len(questions_left) == 0:
         result = sorted(
             probabilities, key = lambda p: p['probability'], reverse = True)[0]
         return render_template('index.html', result = result['name'])
     else:
         next_question = random.choice(questions_left)
-        return render_template('index.html', question=next_question, question_text=questions[next_question])
+        return render_template('index.html', question = next_question, question_text = questions[next_question])
 
 if __name__ == '__main__':
     app.run()
